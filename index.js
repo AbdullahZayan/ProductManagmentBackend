99fb4bac1ed7df73032093307768d3d36c192c68
Abdullah <abdullahzayan.my@gmail.com>
2025-06-05 02:28:49 +0800

    Added Api

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", process.env.API_KEY],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
